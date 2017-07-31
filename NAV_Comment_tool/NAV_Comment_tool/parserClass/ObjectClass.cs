6b1dde29991695f8c1d16381a0acd1ea6403c3d7
CFLightning <cfflightning@gmail.com>
2017-07-31 14:04:21 +0200

    NAV Object Class added

---
+++ NAV_Comment_tool/NAV_Comment_tool/parserClass/ObjectClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAV_Comment_tool.parserClass.ObjectClass
+{
+    class ObjectClass
+    {
+        private int number;
+        private int type;
+        private string name;
+        // private List<ChangeClass>
+
+        public int Number { get => number; set => number = value; }
+        public int Type { get => type; set => type = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
