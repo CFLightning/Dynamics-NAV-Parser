88df35af93fcc49c2dd43278f58de2b7da52ca18
MicDlu <dlugosz.michal.praca@gmail.com>
2017-08-01 12:36:07 +0200

    Solution fix

--- NAV_Comment_tool/NAV_Comment_tool/Program.cs
+++ NAV_Comment_tool/NAV_Comment_tool/Program.cs
@@ -16,18 +16,5 @@ namespace NAV_Comment_tool
             FileSplitter.splitFile(@"C:\Users\Administrator\Documents\Exported example objects\Objects.txt");
             ObjectClassRepository.listAll();
         }
-
-        private static List<string> ImportFromTxt()
-        {
-            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Administrator\Documents\GIt\NAV_Comment_tool\NAV_Comment_tool\TEMP");//Assuming Test is your Folder
-            FileInfo[] Files = d.GetFiles("*.txt"); //Getting Text files
-            List<string> filesList = new List<string>();
-            foreach (FileInfo file in Files)
-            {
-                filesList.Add(file.Name);
-                Console.WriteLine("{0}.\t{1}", filesList.Count, file.Name);
-            }
-            return filesList;
-        }
     }
 }
