3e7d3d06ac141b16e5bdeff3f98e73b68a5cf397
MicDlu <dlugosz.michal.praca@gmail.com>
2017-08-02 14:39:15 +0200

    fix

--- NAV_Comment_tool/NAV_Comment_tool/fileSplitter/ExtractObjectParts.cs
+++ NAV_Comment_tool/NAV_Comment_tool/fileSplitter/ExtractObjectParts.cs
@@ -8,49 +8,54 @@ namespace NAV_Comment_tool.fileSplitter
 {
     class ExtractObjectParts
     {
-        private string text;
-        private string[] textLines;
+        private string objectText;
+        private string[] objectTextLines;
+
+        string[] part_objectProperties;
+        string[] part_properties;
+        string[] part_fields;
+        string[] part_keys;
+        string[] part_fieldgroups;
+        string[] part_code;
+
         string title_type;
         int title_no;
         string title_name;
-        string part_object_properties;
-        string part_properties;
-        string part_fields;
-        string part_keys;
-        string part_fieldgroups;
-        string part_code;
+        List<string> versionList;
 
 
+        //fileSplitter.ExtractObjectParts Obj = new ExtractObjectParts(@"C:\Users\Administrator\Documents\GIt\NAV_Comment_tool\NAV_Comment_tool\TEMP\18cust.txt");
         public ExtractObjectParts(string path)
         {
             LoadFile(path);
+            ExtractParts();
+            FillTitleInfo();
+            FillVersionList();
+            //FindPropertiesTriggers();
         }
 
         public void LoadFile(string path)
         {
-            text = System.IO.File.ReadAllText(@"C:\Users\Administrator\Documents\GIt\NAV_Comment_tool\NAV_Comment_tool\TEMP\tab5.txt");
-            textLines = text.Replace("\r", "").Split('\n');
+            objectText = System.IO.File.ReadAllText(path);
+            objectTextLines = objectText.Replace("\r", "").Split('\n');
         }
 
-        public void GetTitleInfo()
+
+        public void FillTitleInfo()
         {
-            string line = textLines[0];
+            string line = objectTextLines[0];
             string[] titleElements = line.Split();
             title_type = titleElements[1];
             title_no = Int32.Parse(titleElements[2]);
             title_name = titleElements[3];
             for (int i = 4; i < titleElements.Length; i++)
                 title_name += " " + titleElements[i];
-
-            //Console.WriteLine(line);
-            //Console.WriteLine("type: " + type);
-            //Console.WriteLine("no. : " + no);
-            //Console.WriteLine("name: " + name);
         }
 
         public void ExtractParts()
         {
-            string[] lines = text.Replace("\r", "").Split('\n');
+            List<string> tempPartLines = new List<string>();
+            string[] lines = objectText.Replace("\r", "").Split('\n');
             int i = 1;
             do
             {
@@ -58,35 +63,84 @@ namespace NAV_Comment_tool.fileSplitter
             } while (lines[++i] != "  OBJECT-PROPERTIES");
             do
             {
-                part_object_properties += lines[i] + Environment.NewLine;
+                tempPartLines.Add(lines[i]);
             } while (lines[++i] != "  PROPERTIES");
+            part_objectProperties = tempPartLines.ToArray();
+            tempPartLines.Clear();
             do
             {
-                part_properties += lines[i] + Environment.NewLine;
+                tempPartLines.Add(lines[i]);
             } while (lines[++i] != "  FIELDS");
+            part_properties = tempPartLines.ToArray();
+            tempPartLines.Clear();
             do
             {
-                part_fields += lines[i] + Environment.NewLine;
+                tempPartLines.Add(lines[i]);
             } while (lines[++i] != "  KEYS");
+            part_fields = tempPartLines.ToArray();
+            tempPartLines.Clear();
             do
             {
-                part_keys += lines[i] + Environment.NewLine;
+                tempPartLines.Add(lines[i]);
             } while (lines[++i] != "  FIELDGROUPS");
+            part_keys = tempPartLines.ToArray();
             do
             {
-                part_fieldgroups += lines[i] + Environment.NewLine;
+                tempPartLines.Add(lines[i]);
             } while (lines[++i] != "  CODE");
+            part_fieldgroups = tempPartLines.ToArray();
+            tempPartLines.Clear();
             do
             {
-                part_code += lines[i] + Environment.NewLine;
+                tempPartLines.Add(lines[i]);
             } while (++i != lines.Length);
+            part_code = tempPartLines.ToArray();
+            tempPartLines.Clear();
 
-            //Console.Write(object_properties);
-            //Console.Write(properties);
-            //Console.Write(fields);
-            //Console.Write(keys);
-            //Console.Write(object_properties);
-            //Console.Write(code);
+            //foreach (var item in part_code)
+            //    Console.WriteLine(item);
+        }
+
+        private void FillVersionList()
+        {
+            foreach (var line in part_objectProperties)
+            {
+                if (line.Contains("Version List="))
+                {
+                    versionList = line.Substring(line.IndexOf('=')+1).Split(',').ToList();
+                }
+            }
+            versionList[versionList.Count - 1] = versionList.Last().Substring(0, versionList.Last().Length - 1);
+        }
+
+        private void FindPropertiesTriggers()
+        {
+            string[] triggerList = new string[4] {
+                "OnInsert",
+                "OnModify",
+                "OnDelete",
+                "OnRename" };
+            int[] triggerPosition = new int[4];
+            int triggerIdx = 0;
+
+            for (int i = 0; i < part_properties.Length; i++)
+            {
+                if (triggerIdx < 4 && part_properties[i].StartsWith("    " + triggerList[triggerIdx] + "="))
+                {
+                    while (!part_properties[i].Contains("BEGIN")) i++;
+
+                    triggerPosition[triggerIdx] = i;
+                    triggerIdx++;
+                }
+            }
+            
+            for (triggerIdx = 0; triggerIdx < 4; triggerIdx++)
+            {
+                for (int line = triggerPosition[triggerIdx]; part_properties[line-1] != "             END;"; line++)
+                {
+                    Console.WriteLine(triggerList[triggerIdx] + ":" + part_properties[line]);
+                }
+            }
         }
     }
 }
