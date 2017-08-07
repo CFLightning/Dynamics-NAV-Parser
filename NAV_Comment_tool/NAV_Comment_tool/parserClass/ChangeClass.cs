606481bc986e5f5268b31c7316ae9ec9ca43d0f0
CFLightning <cfflightning@gmail.com>
2017-08-07 14:34:10 +0200

    changed saving

--- NAV_Comment_tool/NAV_Comment_tool/parserClass/ChangeClass.cs
+++ NAV_Comment_tool/NAV_Comment_tool/parserClass/ChangeClass.cs
@@ -6,20 +6,23 @@
         private string contents;
         private string changeType;  // code, field, maybe function
         private string location;    //  trigger name or function name
+        private string sourceObject;
 
         public ChangeClass()
         {
         }
-        public ChangeClass(string code, string content, string type, string name)
+        public ChangeClass(string code, string content, string type, string name, string sObject)
         {
             this.ChangelogCode = code;
             this.contents = content;
             this.changeType = type;
             this.location = name;
+            this.sourceObject = sObject;
         }
         public string Contents { get => contents; set => contents = value; }
         public string ChangelogCode { get => changelogCode; set => changelogCode = value; }
         public string ChangeType { get => changeType; set => changeType = value; }
         public string Location { get => location; set => location = value; }
+        public string SourceObject { get => sourceObject; set => sourceObject = value; }
     }
 }
--- NAV_Comment_tool/NAV_Comment_tool/saveTool/SaveTool.cs
+++ NAV_Comment_tool/NAV_Comment_tool/saveTool/SaveTool.cs
@@ -26,8 +26,10 @@ namespace NAV_Comment_tool.saveTool
             {
                 if(chg.ChangeType != "Field")
                 {
-                     File.AppendAllText(path + chg.ChangelogCode + " .txt", chg.Contents);
-                     File.AppendAllText(path + chg.ChangelogCode + " .txt", Environment.NewLine + Environment.NewLine);
+                    File.AppendAllText(path + chg.ChangelogCode + " .txt", "Source object: " + chg.SourceObject + Environment.NewLine);
+                    File.AppendAllText(path + chg.ChangelogCode + " .txt", "Change location: " + chg.Location + Environment.NewLine + Environment.NewLine);
+                    File.AppendAllText(path + chg.ChangelogCode + " .txt", chg.Contents);
+                    File.AppendAllText(path + chg.ChangelogCode + " .txt", Environment.NewLine + Environment.NewLine);
                 }
             }
             return true;
