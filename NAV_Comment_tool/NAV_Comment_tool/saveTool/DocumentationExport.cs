d193e545310198d7db068392023002c93d6bcb01
CFLightning <cfflightning@gmail.com>
2017-08-08 10:13:25 +0200

    generating documentation from automated documentation

--- NAV_Comment_tool/NAV_Comment_tool/saveTool/DocumentationExport.cs
+++ NAV_Comment_tool/NAV_Comment_tool/saveTool/DocumentationExport.cs
@@ -2,8 +2,10 @@
 using NAV_Comment_tool.repositories;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,16 +13,81 @@ namespace NAV_Comment_tool.saveTool
 {
     class DocumentationExport
     {
-        enum Types { Table, Codeunit }; // Add actual numbers for each of the parameters
+        enum Types { Table=1, Page=3, Codeunit }; // Add actual numbers for each of the parameters
 
         public static bool GenerateDocumentationFile()
         {
             Types result;
             foreach (ObjectClass obj in ObjectClassRepository.objectRepository)
-            { 
-                if(Enum.TryParse(obj.Type, out result))
+            {
+                StringReader reader = new StringReader(obj.Contents);
+                StringBuilder builder = new StringBuilder();
+                StringWriter writer = new StringWriter(builder);
+                bool bracketFlag = false, beginFlag = false, writing = false, isOneLine = false, documentationPrompt = false;
+                string line, tagLine = "", trimmer;
+                Regex lineChecker = new Regex(".*#.*#.*");
+                Regex blockChecker = new Regex(".*#.*#$");
+                int lineAmount = 1;
+                if (Enum.TryParse(obj.Type, out result))
                 {
-                    Console.WriteLine("Converted {0} to {1}", obj.Type, (int)result);
+                    //Console.WriteLine("1<next>{0}<next>{1}<next>#tagtagtag#<next> tresc", (int)result, obj.Name);
+                    while (null != (line = reader.ReadLine()))
+                    {
+                        if (line.StartsWith("    BEGIN"))
+                        {
+                            beginFlag = true;
+                        }
+
+                        if (line.StartsWith("      Automated Documentation"))
+                        {
+                            documentationPrompt = true;
+                            writing = true;
+                            continue;
+                        }
+
+                        if (line.StartsWith("    {") && beginFlag)
+                        {
+                            bracketFlag = true;
+                        }
+                       
+                        if (line.StartsWith("    }") && bracketFlag)
+                        {
+                            bracketFlag = false;
+                            writing = false;
+                        }
+
+                        if (line.StartsWith("    END") && beginFlag)
+                        {
+                            beginFlag = false;
+                        }
+
+                        if(writing)
+                        {
+                            if (line.Length > 6)
+                            {
+                                line = line.Substring(6);
+                                
+                                trimmer = line.TrimStart(' ');
+                                trimmer = trimmer.TrimEnd(' ');
+                                if(blockChecker.IsMatch(trimmer)) {
+                                    tagLine = trimmer;
+                                }
+                                else if (lineChecker.IsMatch(trimmer))
+                                {
+                                    trimmer = trimmer.Substring(0, trimmer.LastIndexOf("#") + 1);
+                                    tagLine = trimmer;
+                                    isOneLine = true;
+                                }
+                                Console.WriteLine("{3}<next>{0}<next>{1}<next>{4}<next>{2}", (int)result, obj.Name, line, lineAmount, tagLine);
+                                lineAmount++;
+                                if (isOneLine)
+                                {
+                                    tagLine = "";
+                                    isOneLine = false;
+                                }
+                            }
+                        }
+                    }
                 }
             }
             return true;
